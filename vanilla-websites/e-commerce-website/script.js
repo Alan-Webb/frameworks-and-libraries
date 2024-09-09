a0e61db8f0bf856deb12a86f3c1990f91bd6e7de
Alan Webb <webbalan91939@gmail.com>
2024-09-09 19:13:01 +0100

    Reorganize files

--- vanilla-websites/e-commerce-website/script.js
+++
@@ -1,9 +0,0 @@
-// JS representation from DOM
-const wrapper = document.querySelector(".slider-wrapper");
-const menuItemns = document.querySelectorAll(".menu-item");
-
-menuItemns.forEach((item, index) => {
-	item.addEventListener("click", () => {
-		console.log("clicky boi");
-	});
-});
