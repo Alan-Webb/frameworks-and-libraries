9f19fc4aa3fc3728d977c68cf4c3193f4bd3fe6a
Alan Webb <webbalan91939@gmail.com>
2024-09-09 15:23:58 +0100

    Add eventlistener to menu items

--- vanilla-websites/e-commerce-website/script.js
+++ vanilla-websites/e-commerce-website/script.js
@@ -1,3 +1,9 @@
 // JS representation from DOM
 const wrapper = document.querySelector(".slider-wrapper");
 const menuItemns = document.querySelectorAll(".menu-item");
+
+menuItemns.forEach((item, index) => {
+	item.addEventListener("click", () => {
+		console.log("clicky boi");
+	});
+});
