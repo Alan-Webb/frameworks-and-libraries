4fdaf7a5854dca24c517a153404cb215b855d50f
Alan Webb <webbalan91939@gmail.com>
2024-09-10 01:00:56 +0100

    Add event listener to hamburger toggle

--- vanilla-websites/shop.co/script.js
+++ vanilla-websites/shop.co/script.js
@@ -2,3 +2,8 @@
 const menu = document.querySelector("#menu-icon");
 const navlist = document.querySelector(".navlist");
 
+// Event listeners
+menu.addEventListener("click", () => {
+	menu.classList.toggle("bx-x");
+	navlist.classList.toggle("open");
+});
